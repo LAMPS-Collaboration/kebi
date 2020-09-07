1f635419dc75c2a09eca43aecccf3a30c6e6ce0a
Sanghoon Lim <shlim@pusan.ac.kr>
2020-09-08 01:24:27 +0900

    update more active area

--- LAMPS-HighEnergy/geant4/TB20ADetectorConstruction.cc
+++ LAMPS-HighEnergy/geant4/TB20ADetectorConstruction.cc
@@ -17,6 +17,7 @@
 #include "G4TransportationManager.hh"
 #include "G4UniformMagField.hh"
 #include "G4GlobalMagFieldMessenger.hh"
+#include "G4SubtractionSolid.hh"
 
 TB20ADetectorConstruction::TB20ADetectorConstruction()
 : G4VUserDetectorConstruction()
@@ -36,21 +37,27 @@ G4VPhysicalVolume *TB20ADetectorConstruction::Construct()
 	G4double tpcInnerRadius = par -> GetParDouble("TPCrMin");
 	G4double tpcOuterRadius = par -> GetParDouble("TPCrMax");
 	G4double tpcLength = par -> GetParDouble("TPCLength");
-  G4double tpcZOffset = par -> GetParDouble("zOffset");
-
-  G4double bfieldx = par -> GetParDouble("bfieldx");
-  G4double bfieldy = par -> GetParDouble("bfieldy");
-  G4double bfieldz = par -> GetParDouble("bfieldz");
+	G4double tpcZOffset = par -> GetParDouble("TPCzOffset");
 
   G4NistManager *nist = G4NistManager::Instance();
   G4double STPTemperature = 273.15;
   G4double labTemperature = STPTemperature + 20.*kelvin;
   
-  G4Element *elementH = new G4Element("elementH", "H", 1., 1.00794*g/mole);
-  G4Element *elementC = new G4Element("elementC", "C", 6., 12.011*g/mole);
+	G4Element *elementH = new G4Element("elementH", "H", 1., 1.00794*g/mole);
+	G4Element *elementC = new G4Element("elementC", "C", 6., 12.011*g/mole);
+	//G4Element *elementCu = new G4Element("elementCu","Cu", 29., 63.546*g/mole);
+	//G4Element *elementAl = new G4Element("elementAl","Al", 13., 26.982*g/mole);
 
   G4double densityArGas = 1.782e-3*g/cm3*STPTemperature/labTemperature;
   G4Material *matArGas = new G4Material("ArgonGas", 18, 39.948*g/mole, densityArGas, kStateGas, labTemperature);
+
+	G4Material *matCH2 = new G4Material("CH2", 0.91*g/cm3, 2);
+	matCH2->AddElement(elementH,2);
+	matCH2->AddElement(elementC,1);
+
+	G4Material *matAl = nist->FindOrBuildMaterial("G4_Al");
+	G4Material *matFe = nist->FindOrBuildMaterial("G4_Fe");
+	G4Material *matSC = nist->FindOrBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE");
  
   G4double densityMethane = 0.717e-3*g/cm3*STPTemperature/labTemperature;
   G4Material *matMethaneGas = new G4Material("matMethaneGas ", densityMethane, 2, kStateGas, labTemperature);
@@ -82,17 +89,97 @@ G4VPhysicalVolume *TB20ADetectorConstruction::Construct()
 
   G4Material *matAir = nist -> FindOrBuildMaterial("G4_AIR");
 
-
   G4double worlddX = par -> GetParDouble("worlddX");
   G4double worlddY = par -> GetParDouble("worlddY");
   G4double worlddZ = par -> GetParDouble("worlddZ");
 
-
+	//world
   G4Box *solidWorld = new G4Box("World", worlddX, worlddY, worlddZ);
   G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, matAir, "World");
   G4PVPlacement *physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "World", 0, false, -1, true);
 
 
+	//Collimator
+	G4double Collimatorx = par -> GetParDouble("Collimatorx");
+	G4double Collimatory = par -> GetParDouble("Collimatory");
+	G4double Collimatorz = par -> GetParDouble("Collimatorz");
+	G4double CollimatorzOffset = par -> GetParDouble("CollimatorzOffset");
+
+	G4double Holex = par -> GetParDouble("Holex");
+	G4double Holey = par -> GetParDouble("Holey");
+
+	G4Box *solidCollimator = new G4Box("Collimator", Collimatorx/2.0, Collimatory/2.0, Collimatorz/2.0);
+	G4Box *solidHole = new G4Box("Hole", Holex/2.0, Holey/2.0, Collimatorz/2.0);
+	G4SubtractionSolid *solidSubC = new G4SubtractionSolid("SubC", solidCollimator, solidHole, 0, G4ThreeVector(0,0,0));
+	G4LogicalVolume *logicSubC = new G4LogicalVolume(solidSubC, matFe, "SubC");
+	{
+		G4VisAttributes * attSubC = new G4VisAttributes(G4Colour(G4Colour::Yellow()));
+		logicSubC -> SetVisAttributes(attSubC);
+	}
+	new G4PVPlacement(0, G4ThreeVector(0,0,CollimatorzOffset), logicSubC, "SubC", logicWorld, false, 0, true);
+
+	//Start counter
+	G4Box *solidSC = new G4Box("SC", 200/2.0, 200/2.0, 0.2/2.0);
+	G4LogicalVolume *logicSC = new G4LogicalVolume(solidSC, matSC, "SC");
+	{
+		G4VisAttributes * attSC = new G4VisAttributes(G4Colour(G4Colour::Blue()));
+		logicSC -> SetVisAttributes(attSC);
+	}
+	new G4PVPlacement(0, G4ThreeVector(0,0,-150), logicSC, "SC", logicWorld, false, 0, true);
+
+	G4Box *solidVeto = new G4Box("Veto", 200/2.0, 200/2.0, 0.2/2.0);
+	G4LogicalVolume *logicVeto = new G4LogicalVolume(solidVeto, matSC, "Veto");
+	{
+		G4VisAttributes * attVeto = new G4VisAttributes(G4Colour(G4Colour::Blue()));
+		logicVeto -> SetVisAttributes(attVeto);
+	}
+	new G4PVPlacement(0, G4ThreeVector(0,0,-100), logicVeto, "Veto", logicWorld, false, 0, true);
+
+
+	//Target1
+	G4double Target1x = par -> GetParDouble("Target1x");
+	G4double Target1y = par -> GetParDouble("Target1y");
+	G4double Target1z = par -> GetParDouble("Target1z");
+	G4double Target1zOffset = par -> GetParDouble("Target1zOffset");
+
+	G4Box *solidTarget1 = new G4Box("Target1", Target1x/2.0, Target1y/2.0, Target1z/2.0);
+	G4LogicalVolume *logicTarget1 = new G4LogicalVolume(solidTarget1, matCH2, "Traget1");
+	{
+		G4VisAttributes * attTarget1 = new G4VisAttributes(G4Colour(G4Colour::Green()));
+		logicTarget1 -> SetVisAttributes(attTarget1);
+	}
+	new G4PVPlacement(0, G4ThreeVector(0,0,Target1zOffset), logicTarget1, "Target1", logicWorld, false, 0, true);
+
+	//Target2
+	G4double Target2x = par -> GetParDouble("Target2x");
+	G4double Target2y = par -> GetParDouble("Target2y");
+	G4double Target2z = par -> GetParDouble("Target2z");
+	G4double Target2zOffset = par -> GetParDouble("Target2zOffset");
+
+	G4Box *solidTarget2 = new G4Box("Target2", Target2x/2.0, Target2y/2.0, Target2z/2.0);
+	G4LogicalVolume *logicTarget2 = new G4LogicalVolume(solidTarget2, matCH2, "Traget1");
+	{
+		G4VisAttributes * attTarget2 = new G4VisAttributes(G4Colour(G4Colour::Green()));
+		logicTarget2 -> SetVisAttributes(attTarget2);
+	}
+	new G4PVPlacement(0, G4ThreeVector(0,0,Target2zOffset), logicTarget2, "Target2", logicWorld, false, 0, true);
+
+	//Target3
+	G4double Target3x = par -> GetParDouble("Target3x");
+	G4double Target3y = par -> GetParDouble("Target3y");
+	G4double Target3z = par -> GetParDouble("Target3z");
+	G4double Target3zOffset = par -> GetParDouble("Target3zOffset");
+
+	G4Box *solidTarget3 = new G4Box("Target3", Target3x/2.0, Target3y/2.0, Target3z/2.0);
+	G4LogicalVolume *logicTarget3 = new G4LogicalVolume(solidTarget3, matCH2, "Traget1");
+	{
+		G4VisAttributes * attTarget3 = new G4VisAttributes(G4Colour(G4Colour::Green()));
+		logicTarget3 -> SetVisAttributes(attTarget3);
+	}
+	new G4PVPlacement(0, G4ThreeVector(0,0,Target3zOffset), logicTarget3, "Target3", logicWorld, false, 0, true);
+
+
+	//TPC
   G4Tubs *solidTPC = new G4Tubs("TPC", tpcInnerRadius, tpcOuterRadius, .5*tpcLength, 0., 360*deg);
   G4LogicalVolume *logicTPC = new G4LogicalVolume(solidTPC, matGas, "TPC");
   {
@@ -106,6 +193,7 @@ G4VPhysicalVolume *TB20ADetectorConstruction::Construct()
 
 
 
+	/*
   bool checkWall = par -> CheckPar("numNeutronWall");
   if (checkWall)
   {
@@ -138,8 +226,12 @@ G4VPhysicalVolume *TB20ADetectorConstruction::Construct()
       }
     }
   }
+	*/
 
-  new G4GlobalMagFieldMessenger(G4ThreeVector(bfieldx*tesla, bfieldy*tesla, bfieldz*tesla));
+  //G4double bfieldx = par -> GetParDouble("bfieldx");
+  //G4double bfieldy = par -> GetParDouble("bfieldy");
+  //G4double bfieldz = par -> GetParDouble("bfieldz");
+  //new G4GlobalMagFieldMessenger(G4ThreeVector(bfieldx*tesla, bfieldy*tesla, bfieldz*tesla));
 
   return physWorld;
 }
