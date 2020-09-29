ad986a36b8e2afa2f0f6c730491019daeaa553b4
Sanghoon Lim <shlim@pusan.ac.kr>
2020-09-29 12:56:46 +0900

    update to set physics list

--- LAMPS-HighEnergy/macros_tpc/run_TB20Amc.cc
+++ LAMPS-HighEnergy/macros_tpc/run_TB20Amc.cc
@@ -3,10 +3,10 @@
 
 #include "KBG4RunManager.hh"
 #include "G4StepLimiterPhysics.hh"
+#include "G4PhysListFactory.hh"
 
 #include "KBParameterContainer.hh"
 
-#include "QGSP_BERT.hh"
 #include "TB20ADetectorConstruction.hh"
 #include "KBPrimaryGeneratorAction.hh"
 
@@ -18,7 +18,21 @@ int main(int argc, char** argv)
 {
   auto runManager = new KBG4RunManager();
 
-  G4VModularPhysicsList* physicsList = new QGSP_BERT;
+	std::string _PhysList;
+	if ( argc==2 ){
+		_PhysList = "QGSP_BERT";
+		cout << "No PhysList is specfied. Set " << _PhysList << endl;
+	}else if ( argc==3 ){
+		_PhysList = argv[2];
+		cout << "Set " << _PhysList << endl;
+	}else{
+		cout << "Usage: ./run_TB20Amc.g4sim file.conf PhysList" << endl;
+		return 0;
+	}
+
+
+	G4PhysListFactory *physListFactory = new G4PhysListFactory();
+	G4VModularPhysicsList* physicsList = physListFactory->GetReferencePhysList(_PhysList.c_str());
   physicsList -> RegisterPhysics(new G4StepLimiterPhysics());
   runManager -> SetUserInitialization(physicsList);
   runManager -> SetParameterContainer(argv[1]);
