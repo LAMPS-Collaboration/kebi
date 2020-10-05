0a72487de9ef2b4fa961d851033930c954704630
lamps@kunpl <phyjics@gmail.com>
2020-10-05 22:55:06 +0900

    ProtytypeTPC run_reco.C bug fix.

--- PrototypeTPC/macros/run_reco.C
+++ PrototypeTPC/macros/run_reco.C
@@ -1,9 +1,8 @@
-//void run_reco(Int_t runID = 98, Int_t split = 0, Int_t numEventsPerSplit=2000)
-void run_reco(Int_t runID = 98, Int_t split = 0, Int_t numEventsPerSplit=2000)
+void run_reco(Int_t runID = 98, Int_t split = 0, Int_t numEventsPerSplit=20)
 {
   auto run = new KBRun();
   run -> SetRunID(runID);
-  run -> SetTag("reco");
+  run -> SetTag("test");
   run -> SetSplit(split, numEventsPerSplit);
   run -> AddPar("prototype.par");
   run -> AddDetector(new LAPTpc());
@@ -13,11 +12,14 @@ void run_reco(Int_t runID = 98, Int_t split = 0, Int_t numEventsPerSplit=2000)
 
   string line;
   Int_t runID0, numEventsNalval=0, numEventsVME=0;
-  ifstream summary(run -> GetPar() -> GetParString("expRunSummary"));
-  getline(summary,line);
-  getline(summary,line);
-  getline(summary,line);
-  while (getline(summary,line)) {
+  std::ifstream summary(run -> GetPar() -> GetParString("expRunSummary"));
+  std::getline(summary,line);
+  std::getline(summary,line);
+  std::getline(summary,line);
+  while (1) {
+    std::getline(summary,line);
+    if (line.empty())
+      break;
     istringstream ss(line);
     ss >> runID0;
     if (runID0==runID) {
